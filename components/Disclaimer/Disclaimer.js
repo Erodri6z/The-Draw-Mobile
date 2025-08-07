b8b6ef61eaada3ff8a9760eefb8c4f0e5f2795e7
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-07 13:56:57 -0500

    stub up the dsisclaimer modal

---
+++ components/Disclaimer/Disclaimer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Modal, Text, View, TouchableOpacity } from "react-native";
+
+function DisclaimerModel({ onAccept }) {
+  return (
+    <Modal transparent={true} animationType="fade" visible={true}>
+      <View>
+        <View>
+          <Text>
+
+          </Text>
+          <Text>
+
+          </Text>
+          <TouchableOpacity>
+            <Text></Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  )
+}
