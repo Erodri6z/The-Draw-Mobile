fc15681e1debb9d19ac955993cb66ffef82d2846
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-07 14:06:51 -0500

    add text to the diclaimer model

--- components/Disclaimer/Disclaimer.js
+++ components/Disclaimer/Disclaimer.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Modal, Text, View, TouchableOpacity } from "react-native";
+
+import { Modal, Text, View, TouchableOpacity } from "react-native"
 
 function DisclaimerModel({ onAccept }) {
   return (
@@ -7,16 +7,18 @@ function DisclaimerModel({ onAccept }) {
       <View>
         <View>
           <Text>
-
+            DisclaimerModel
           </Text>
           <Text>
-
+            This tarot reading app is intended for entertainment, reflection, and personal insight purposes only. The information provided through tarot card readings should not be considered medical, legal, financial, or professional advice. This app uses artificial intelligence powered by OpenAI to generate interpretations and messages. These are created automatically and are not written or reviewed by a human. By using this app, you agree that you are solely responsible for how you interpret and act on any reading. The creators of this app are not liable for any decisions, outcomes, or emotional responses resulting from use.If you're facing serious emotional, medical, or legal concerns, please consult a qualified professional.
           </Text>
           <TouchableOpacity>
-            <Text></Text>
+            <Text>I Understand</Text>
           </TouchableOpacity>
         </View>
       </View>
     </Modal>
   )
 }
+
+export default DisclaimerModel
