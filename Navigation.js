3d37928bafcb6496283695ea97d2469d5daf1cf5
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-07 20:42:39 -0500

    figure out the navigation

--- Navigation.js
+++ Navigation.js
@@ -1,20 +1,41 @@
-// Navigation.js
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs' 
 import Landing from './screens/LandingScreen/LandingScreen';
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
-export default function Navigation(props) {
+
+function LandingWrapper(props) {
+  return <Landing user={props.user} />;
+}
+
+
+function Navigation(props) {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Landing">
-          {() => <Landing user={props.user} />}
-        </Stack.Screen>
+        <Stack.Screen name="MainTabs" children={() => <TabNavigation {...props} />} />
+        <Stack.Screen name="Landing" children={() => <LandingWrapper {...props} />} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
+
+function TabNavigation(props) {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }} >
+      <Tab.Screen name="Landing" 
+      children={() => <LandingWrapper {...props} />} />
+
+      {/* <Tab.Screen>
+      </Tab.Screen> */}
+    </Tab.Navigator>
+  )
+}
+
+
+
+export default Navigation 
