cc46c0cf57037980fe931328b48edb17cf2409be
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-09 18:41:27 -0500

    add a view for the nav styles

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs' 
 import Landing from '../../screens/LandingScreen/LandingScreen';
+import { View } from 'react-native';
 const Tab = createBottomTabNavigator()
 
 
@@ -9,13 +10,15 @@ function LandingWrapper(props) {
 
 function TabNavigation(props) {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} >
-      <Tab.Screen name="Landing" 
-      children={() => <LandingWrapper {...props} />} />
+    <View >
+      <Tab.Navigator screenOptions={{ headerShown: false }} >
+        <Tab.Screen name="Landing" 
+        children={() => <LandingWrapper {...props} />} />
 
-      {/* <Tab.Screen>
-      </Tab.Screen> */}
-    </Tab.Navigator>
+        {/* <Tab.Screen>
+        </Tab.Screen> */}
+      </Tab.Navigator>
+    </View>
   )
 }
 
