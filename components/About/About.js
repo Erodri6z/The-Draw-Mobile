588d6ed3e2da82ec3b9ef8344300acdbc6b78b09
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-18 21:08:29 -0500

    stub up the about component

---
+++ components/About/About.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { ScrollView, Text, View } from "react-native-gesture-handler"
+
+const About = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text>The Draw</Text>
+      </View>
+
+      <View>
+        <Text>
+          What are tarot cards
+        </Text>
+        <Text>
+          Section One 
+        </Text>
+      </View>
+
+      <View>
+        <Text>
+          Explination
+        </Text>
+        <Text>
+          Section 2
+        </Text>
+      </View>
+
+      <View>
+        <Text>
+          There is no rules, it's just an invitation
+        </Text>
+        <Text>
+          Section 3
+        </Text>
+      </View>
+
+      <View>
+        <Text>
+          Best Practice :
+        </Text>
+        <Text>
+          Section 4
+        </Text>
+      </View>
+
+    </ScrollView>
+  )
+}
+
+export default About
